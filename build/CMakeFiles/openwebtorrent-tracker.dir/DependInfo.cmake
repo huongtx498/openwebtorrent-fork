3a6c06ab046da2012716055ca0f324db68f765cd
trinhhuong244 <trinhhuong244@gmail.com>
2021-01-07 00:09:02 +0700

    build version

---
+++ build/CMakeFiles/openwebtorrent-tracker.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/src/main.cpp" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/openwebtorrent-tracker.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LIBUS_USE_OPENSSL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../deps/uWebSockets/src"
+  "../deps/json/single_include/nlohmann"
+  "../deps/cxxopts/include"
+  "../deps/uWebSockets/uSockets/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uWebSockets.dir/DependInfo.cmake"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/uSockets.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/bsd.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/bsd.c.o"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/context.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/context.c.o"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/crypto/openssl.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/crypto/openssl.c.o"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/crypto/wolfssl.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/crypto/wolfssl.c.o"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/eventing/epoll_kqueue.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/eventing/epoll_kqueue.c.o"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/eventing/gcd.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/eventing/gcd.c.o"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/eventing/libuv.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/eventing/libuv.c.o"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/loop.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/loop.c.o"
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/socket.c" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/socket.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "LIBUS_USE_OPENSSL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../deps/uWebSockets/uSockets/src"
+  "../deps/uWebSockets/src"
+  "../deps/json/single_include/nlohmann"
+  "../deps/cxxopts/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/deps/uWebSockets/uSockets/src/crypto/sni_tree.cpp" "/home/huongtx/Documents/Projects/Openwebtorrent/openwebtorrent-tracker/build/CMakeFiles/uSockets.dir/deps/uWebSockets/uSockets/src/crypto/sni_tree.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LIBUS_USE_OPENSSL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../deps/uWebSockets/uSockets/src"
+  "../deps/uWebSockets/src"
+  "../deps/json/single_include/nlohmann"
+  "../deps/cxxopts/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
